b7c8ddfc228104b97940769144f6194888c5300d
Michael McClure <salamandermike@gmail.com>
2017-11-21 20:57:44 -0800

    Added Javascript Flicker to Neon Lights

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,7 +1,45 @@
-export default function AppController ($scope) {
+export default function AppController ($scope, $timeout, $q) {
 	'ngInject';
 	var app = this;
+	var i = 5;
+	$scope.glow = false;
+
 	
+	$scope.flicker = function() {
+		$q.when(number()).then(function(duration){
+			$scope.test = duration;
+			$scope.glow = true;
+			$timeout(function() {
+				$scope.glow = false;
+				$timeout(function() {
+					$scope.glow = true;
+					$timeout(function() {
+						$scope.glow = false;
+						$timeout(function() {
+							$scope.glow = true;
+							$timeout(function() {
+								$scope.glow = false;
+							},duration[4]);
+						},duration[3]);
+					},duration[2]);
+				},duration[1]);
+			},duration[0]);
+		});
+	}
+	
+	function number(){
+		var array = [90,100,110,500,1000];
+		var duration = [];
+		while (i--) {
+			var random = Math.floor(Math.random() * (i + 1));
+			duration.push(array[random]);
+			array.splice(random, 1);
+			if (duration.length == 5) {
+				i = 5;
+				return duration;
+			}
+		}
+	}
 
 
 
