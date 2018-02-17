183502a754cd410dcf71707a51d779df25b53936
Michael McClure <salamandermike@gmail.com>
2018-02-16 20:49:08 -0800

    Remove Extraneous Code

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
 				use: [
 				{ 	loader: 'url-loader',
 					options: {
-						limit: 10000,
+						limit: 8000,
 						outputPath: '/public/images/',
 						name: '[name].[ext]'
 					}
