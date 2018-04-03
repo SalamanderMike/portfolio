688571772c801bab5ab5532806b0937af7be863b
Michael McClure <salamandermike@gmail.com>
2018-04-03 16:18:02 -0700

    Rollback To Working Production Commit

--- app/app.js
+++ app/app.js
@@ -10,21 +10,6 @@ import router from './routing';
 import AppController from './controllers/controller.js';
 import API from './services/service.js';
 
-import greyLinen from './styles/img/grey-linen.jpg';
-import greyTile from './styles/img/grey-tile.jpg';
-
-import bug from './styles/img/bug.jpg';
-import culture from './styles/img/culture.jpg';
-import dry from './styles/img/dry.jpg';
-import help from './styles/img/help.jpg';
-import me from './styles/img/me.jpg';
-import ux from './styles/img/ux.jpg';
-import neonArrowBig from './styles/img/neon-arrow-big.png';
-import glowArrowBig from './styles/img/glow-arrow-big.png';
-import neonArrowSmall from './styles/img/neon-arrow-small.png';
-import glowArrowSmall from './styles/img/glow-arrow-small.png';
-import mIconSmall from './styles/img/m-icon-small.png';
-
 
 export default angular.module('app', [ngRoute, ngResource, ngAnimate, uiBootstrap])
 	.service('API', API)
